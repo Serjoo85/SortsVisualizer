4a3a74cde4d1f5d3dd28c9d75413562e3f33981d
Serjo85 <bobrosebob07@gmail.com>
2022-05-12 16:40:51 +0300

    Добавлена загатовка SortsVisualizer.lib для размещения логики приложения.

---
+++ SortsVisualizer.lib/Commands/Base/Command.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace MailSender.lib.Commands.Base
+{
+    abstract public class Command : ICommand
+    {
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        abstract public bool CanExecute(object parameter);
+
+        abstract public void Execute(object parameter);
+    }
+}
