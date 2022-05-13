a58b0b89ee565b973ce199f4ca3ffe4dd0158ebe
Serjo85 <bobrosebob07@gmail.com>
2022-05-13 21:04:36 +0300

    Исправлена ошибка в xaml контрола и добавлена логика.

--- SortsVisualizer.UI/Controls/ArrayVisualizer.xaml.cs
+++ SortsVisualizer.UI/Controls/ArrayVisualizer.xaml.cs
@@ -1,6 +1,13 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 using Rectangle = System.Windows.Shapes.Rectangle;
 
 namespace SortsVisualizer.UI.Controls;
@@ -8,40 +15,95 @@ namespace SortsVisualizer.UI.Controls;
 /// <summary>
 /// Interaction logic for ArrayVisualizer.xaml
 /// </summary>
-public partial class ArrayVisualizer : UserControl
+public partial class ArrayVisualizer : UserControl, INotifyPropertyChanged
 {
+    private static int _heightFactor;
+
     public ArrayVisualizer()
     {
         InitializeComponent();
     }
 
-    private static async void CreateDiagram(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    #region Base functionality INotifyPropertyChanged
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null!)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string propertyName = null!)
+    {
+        if (Equals(field, value)) return false;
+        field = value;
+        OnPropertyChanged(propertyName);
+        return true;
+    }
+
+    #endregion
+
+    private static void CreateDiagram(DependencyObject d, DependencyPropertyChangedEventArgs e)
     {
         var userControl = d as ArrayVisualizer;
         var viewArray = userControl!.ViewArray;
         var array = userControl!.DiagramSource;
+        if (viewArray.Children.Count > 0)
+        {
+            Task.Run(() =>
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    var i1 = i;
+                    viewArray.Dispatcher.Invoke(() => ((Rectangle)viewArray.Children[i1]).Height = array[i1] * _heightFactor);
+                }
+            });
+            return;
+        }
+        var max = array.Max();
+        int width = 1000 / array.Length;
+        _heightFactor = 500 / max;
 
-        await Task.Run(() =>
+        Task.Run(() =>
         {
             viewArray.Dispatcher.Invoke(() => viewArray.Children.Clear());
-            foreach (Rectangle rect in array)
+            foreach (var height in array)
             {
+                var rect = CreateRectangle(height * _heightFactor, width);
                 viewArray.Dispatcher.Invoke(() => viewArray.Children.Add(rect));
+                Debug.WriteLine(Thread.CurrentThread.ManagedThreadId);
             }
         });
     }
 
-    public Rectangle[] DiagramSource
+    public int[] DiagramSource
     {
-        get => (Rectangle[])GetValue(DiagramSourceProperty);
+        get => (int[])GetValue(DiagramSourceProperty);
         set => SetValue(DiagramSourceProperty, value);
     }
 
     public static readonly DependencyProperty DiagramSourceProperty = DependencyProperty.Register
     (
         nameof(DiagramSource),
-        typeof(Rectangle[]),
+        typeof(int[]),
         typeof(ArrayVisualizer),
         new PropertyMetadata(null, CreateDiagram)
         );
+
+    private static Rectangle CreateRectangle(int height, int width)
+    {
+        Rectangle rectangle = null!;
+        Application.Current.Dispatcher.Invoke(() =>
+        {
+            rectangle = new System.Windows.Shapes.Rectangle
+            {
+                Width = width,
+                Height = height,
+                Stroke = Brushes.Black,
+                VerticalAlignment = VerticalAlignment.Bottom,
+            };
+        });
+
+        return rectangle!;
+    }
 }
