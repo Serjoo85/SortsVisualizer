f3391fa6e904e780280655a6027ea3cd137fcad7
Serjo85 <bobrosebob07@gmail.com>
2022-05-13 13:17:30 +0300

    Добавлен интерфейс и заготовка реализации для BubbleSort

---
+++ SortsVisualizer.lib/Services/BubbleSorting.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Shapes;
+using SortsVisualizer.lib.Interfaces;
+
+namespace SortsVisualizer.lib.Services;
+
+internal class BubbleSorting : ISorter
+{
+    public Rectangle[] NextStep(Rectangle[] rectangles)
+    {
+        throw new NotImplementedException();
+    }
+}
