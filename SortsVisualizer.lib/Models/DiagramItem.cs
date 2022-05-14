f0cec606cc0d3673707670878a78a6c3aa60f83f
Serjo85 <bobrosebob07@gmail.com>
2022-05-14 15:49:44 +0300

    Переименован клаас.

---
+++ SortsVisualizer.lib/Models/DiagramItem.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Media;
+
+namespace SortsVisualizer.lib.Models;
+
+public struct DiagramItem
+{
+    public int Value { get; set; }
+    public int Height { get; set; }
+    public int Width { get; set; }
+    public SolidColorBrush Color { get; set; }
+}
