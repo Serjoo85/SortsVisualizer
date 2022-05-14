cbaf09c48969de3f33091cb11a5cdf9bfa8c172e
Serjo85 <bobrosebob07@gmail.com>
2022-05-15 02:45:47 +0300

    Добавлен общий интерфейс для всех сортировок. Создан класс сортировки Bubble с реализованным ISorterStrategy.

---
+++ SortsVisualizer.lib/Interfaces/ISorterStrategy.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.ObjectModel;
+using SortsVisualizer.lib.Models;
+
+namespace SortsVisualizer.lib.Interfaces;
+
+public interface ISorterStrategy
+{
+    Task StartAsync(ObservableCollection<DiagramItem> collection);
+    void Stop();
+}
