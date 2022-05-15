6536c923d7fea1f4bc3506e1243ed157d2247a1b
Serjo85 <bobrosebob07@gmail.com>
2022-05-15 04:02:32 +0300

    Запуск и остановка соритровки убрана внутрь SorterService.

--- SortsVisualizer.lib/Interfaces/ISorterService.cs
+++ SortsVisualizer.lib/Interfaces/ISorterService.cs
@@ -1,9 +1,12 @@
-﻿using SortsVisualizer.lib.Enums;
+﻿using System.Collections.ObjectModel;
+using SortsVisualizer.lib.Enums;
+using SortsVisualizer.lib.Models;
 
 namespace SortsVisualizer.lib.Interfaces;
 
 public interface ISorterService
 {
-    public ISorterStrategy GetSorter(SortType type);
+    public Task StartAsync(SortType type, ObservableCollection<DiagramItem> collection);
+    public void Stop();
     public string[] GetSortersTypes();
 }
