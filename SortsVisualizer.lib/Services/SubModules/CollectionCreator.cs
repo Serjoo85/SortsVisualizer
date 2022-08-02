e4e573d5075bd65b830c0a130b9a744f86edb0b0
Serjo85 <bobrosebob07@gmail.com>
2022-08-02 13:55:48 +0300

    Add two classes for decomposition DiagramService and make it more logical

---
+++ SortsVisualizer.lib/Services/SubModules/CollectionCreator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Media;
+using SortsVisualizer.lib.Models;
+
+namespace SortsVisualizer.lib.Services.SubModules;
+
+public static class CollectionCreator
+{
+    public static ObservableCollection<DiagramItem> GetCollection(int elementsCount = 20, int heightFactor = 25, int width = 50)
+    {
+        var items = new ObservableCollection<DiagramItem>();
+        for (int i = 1; i < elementsCount + 1; i++)
+            items.Add(new DiagramItem
+            {
+                Value = i,
+                Height = i * heightFactor,
+                Width = width,
+                Color = Brushes.White,
+            });
+        return items;
+    }
+}
---
+++ SortsVisualizer.lib/Services/SubModules/ColorChanger.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Windows.Media;
+using SortsVisualizer.lib.Models;
+using SortsVisualizer.lib.Services.Interfaces;
+
+namespace SortsVisualizer.lib.Services.SubModules;
+
+public class ColorChanger : IColorChanger
+{
+    private readonly ObservableCollection<DiagramItem> _items = null!;
+    private readonly Action<NotifyCollectionChangedAction> _onCollectionChanged;
+
+    public ColorChanger(ObservableCollection<DiagramItem> items, Action<NotifyCollectionChangedAction> onCollectionChanged)
+    {
+        _items = items;
+        _onCollectionChanged = onCollectionChanged;
+    }
+
+    public void FillSingleRectangle(
+        int index,
+        System.Windows.Media.Color color)
+    {
+        var newItem = _items[index];
+        newItem.Color = new SolidColorBrush(color);
+        _items[index] = newItem;
+    }
+
+    public void Change(
+        int index,
+        System.Windows.Media.Color color)
+    {
+        var newItem = _items[index];
+        newItem.Color = new SolidColorBrush(color);
+        _items[index] = newItem;
+    }
+
+    public async Task MakeLadderAnimation(
+        CancellationToken cancel,
+        System.Windows.Media.Color color,
+        int delay = 80)
+    {
+        for (int i = 0; i < _items.Count; i++)
+        {
+            if (_items[i].Color.Color == color)
+                continue;
+            Change(i, color);
+            await Task.Delay(delay, cancel);
+            _onCollectionChanged(NotifyCollectionChangedAction.Replace);
+        }
+    }
+}
